216258a3dcdfa5aeaa8dfad6d6fecd48162c9900
Alp Sahin <alp@sahin.club>
2021-08-17 15:37:52 +0300

    Created services and providers for Starred Repos feature

--- lib/auth/shared/providers.dart
+++ lib/auth/shared/providers.dart
@@ -4,18 +4,19 @@ import 'package:repo_viewer/auth/application/auth_notifier.dart';
 import 'package:repo_viewer/auth/infrastructure/credentials_storage/credentials_storage.dart';
 import 'package:repo_viewer/auth/infrastructure/credentials_storage/secure_credentials_storage.dart';
 import 'package:repo_viewer/auth/infrastructure/github_authenticator.dart';
+import 'package:repo_viewer/auth/infrastructure/oauth2_interceptor.dart';
 import 'package:riverpod/riverpod.dart';
 
 final flutterSecureStorageProvider = Provider((ref) => const FlutterSecureStorage());
 
-final dioProvider = Provider((ref) => Dio());
+final dioForAuthProvider = Provider((ref) => Dio());
 
 final credentialsStorageProvider = Provider<CredentialsStorage>(
   (ref) => SecureCredentialsStorage(ref.watch(flutterSecureStorageProvider)),
 );
 
 final githubAuthenticatorProvider = Provider(
-  (ref) => GithubAuthenticator(ref.watch(credentialsStorageProvider), ref.watch(dioProvider)),
+  (ref) => GithubAuthenticator(ref.watch(credentialsStorageProvider), ref.watch(dioForAuthProvider)),
 );
 
 final authNotifierProvider = StateNotifierProvider<AuthNotifier, AuthState>(
@@ -23,3 +24,9 @@ final authNotifierProvider = StateNotifierProvider<AuthNotifier, AuthState>(
     ref.watch(githubAuthenticatorProvider),
   ),
 );
+
+final oAuth2InterceptorProvider = Provider((ref) => OAuth2Interceptor(
+  ref.watch(githubAuthenticatorProvider),
+  ref.watch(authNotifierProvider.notifier),
+  ref.watch(dioForAuthProvider),
+));
---
+++ lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
@@ -0,0 +1,72 @@
+import 'package:dio/dio.dart';
+import 'package:repo_viewer/core/infrastructure/dio_extensions.dart';
+import 'package:repo_viewer/core/infrastructure/network_exceptions.dart';
+import 'package:repo_viewer/core/infrastructure/remote_response.dart';
+import 'package:repo_viewer/github/core/infrastructure/github_headers.dart';
+import 'package:repo_viewer/github/core/infrastructure/github_headers_cache.dart';
+import 'package:repo_viewer/github/core/infrastructure/github_repo_dto.dart';
+import 'package:repo_viewer/github/core/infrastructure/pagination_config.dart';
+
+class StarredReposRemoteService {
+  final Dio _dio;
+  final GithubHeadersCache _headersCache;
+
+  StarredReposRemoteService(
+    this._dio,
+    this._headersCache,
+  );
+
+  Future<RemoteResponse<List<GithubRepoDTO>>> getStarredReposPage(
+    int page,
+  ) async {
+    final requestUri = Uri.https(
+      'api.github.com',
+      '/user/starred',
+      {
+        'page': '$page',
+        'per_page': PaginationConfig.itemsPerPage.toString(),
+      },
+    );
+
+    final previousHeaders = await _headersCache.getHeaders(requestUri);
+
+    try {
+      final response = await _dio.getUri(
+        requestUri,
+        options: Options(
+          headers: {
+            'If-None-Match': previousHeaders?.etag ?? '',
+          },
+        ),
+      );
+
+      if (response.statusCode == 304) {
+        return RemoteResponse.notModified(
+          maxPage: previousHeaders?.link?.maxPage ?? 0,
+        );
+      } else if (response.statusCode == 200) {
+        final headers = GithubHeaders.parse(response);
+        headers.link?.maxPage;
+        await _headersCache.saveHeaders(requestUri, headers);
+        final convertedData =
+            (response.data as List<dynamic>).map((e) => GithubRepoDTO.fromJson(e as Map<String, dynamic>)).toList();
+        return RemoteResponse.withNewData(
+          convertedData,
+          maxPage: headers.link?.maxPage ?? 1,
+        );
+      } else {
+        throw RestApiException(response.statusCode);
+      }
+    } on DioError catch (e) {
+      if (e.isNoConnectionError) {
+        return RemoteResponse.noConnection(
+          maxPage: previousHeaders?.link?.maxPage ?? 0,
+        );
+      } else if (e.response != null) {
+        throw RestApiException(e.response?.statusCode);
+      } else {
+        rethrow;
+      }
+    }
+  }
+}
